88e3d6b11fd7a7418be06517f605eb064b2de931
Tarakani <sergey.murashko21@gmail.com>
2020-09-25 16:55:40 +0300

    removed unused setters

--- product/products/Product.php
+++ product/products/Product.php
@@ -15,16 +15,6 @@ abstract class Product
         return $this->weightOrQuantity;
     }
 
-    public function setCost($cost)
-    {
-        $this->cost = $cost;
-    }
-
-    public function setWeightOrQuantity($weightOrQuantity)
-    {
-        $this->weightOrQuantity = $weightOrQuantity;
-    }
-
     function __construct($cost, $weightOrQuantity){
         $this->cost = $cost;
         $this->weightOrQuantity = $weightOrQuantity;
